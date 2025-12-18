eacca93226ca81edd5adadd2c6a95ca25f91d3f5
Storm <82328097+strumberr@users.noreply.github.com>
2025-12-18 09:57:44 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_key_pair" "main" {
   key_name   = "mykey3"
   ## replace the key with the content from previous command
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDRnZcbfrQeHOd14wjKwzbeh0rWpiOgHQ5X+B7RVIsVSzUGShjJhs/xR9waz1N6R5fy4iCODE8yMAww6x0lGJKasrRmIK/YqTMaqYdnWy43gB3bEiUbwuuzAXzIbRR261A7CtbI0rByvcfyTt3XQRxei5X8gS+dAJVM6pnkXtioiYYLgCKhLWlJSUU6kSpU/ppLZ7ZdgZcV/7xGzZ3esX1KeKLX8cngXYY6IQ8iAm4cGpbe9sZ2lkE3ljwcIWusQ5XmDU3Iu8GugnCEK5mOjKeTuw0H46xGS2QZkfbNFFBaioMIQ17hhdUA9So602GUfPloy1vG3WYDYzKLhFOzCgEFePgtcqMaKQ4x0eeULaoZVT/rw1VwkE9X8Rwscyhv+EwFVxvAplQQA5CPl8PXATL9T+lZiFpwz15jFll04wI18eyB1OGAxhBZ6UV1oeaDPrr1Oveu/Dnix+/Gxaz2wiq535nH/8kNfFSaO6bPcNT5uRzt1i2te7iddPuK7MUbEQjRys5QDnxTxHktOjtfEqRJGzqjNEv7MHSLMB0FelcpKu73NT8JIhUYS1TU2Rp6eVRBQP2xqHB+cHCwOPi7lxugGHSfaN600qwiTacTCtPMi+dCzDc3acPdUh7foV8NiZHG8nKiJQ9nntXG73qCQv2kFLxclWEJeEgJbYkyzAfbcQ== maksymprokopov@maksymprokopov-K2TDG245VV"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM1WfkeyDv3NatOS7lkPiKbdna08FTZk//8cboQrrTHI laborant@flexbox"
 }
 
 resource "aws_security_group" "main" {
